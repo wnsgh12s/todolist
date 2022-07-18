71a7472d4c8463e3b4e6ca8846f70753d39e5d05
junho lee <junho1273@gmail.com>
2022-07-18 17:52:01 +0900

    header_layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header id="header">
+        <div className="header-inner">
+          <h1>Todolist</h1>
+          <nav className="gnb">
+            <ul>
+              <li>오늘 할일</li>
+              <li>내일 할일</li>
+            </ul>
+          </nav>
+        </div>
       </header>
     </div>
-  );
+  );  
 }
 
 export default App;
