ecac1112027fc9fddcbd396640a0e12be9c99628
junho lee <junho1273@gmail.com>
2022-07-22 20:42:15 +0900

    라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react';
 import Main from './component/Main';
 import Header from './component/Header';
 import './App.css';
+import {Route,Routes} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Main/>
+      <Routes>
+        <Route path='/' element={<Main/>}></Route>
+      </Routes>
     </div>  
   );  
 }
