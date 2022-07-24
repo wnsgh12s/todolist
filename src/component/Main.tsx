b0917a0c29486a6b7c9d60843ece04cc7a01694f
junho lee <junho1273@gmail.com>
2022-07-24 21:27:26 +0900

    localStorage

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -1,21 +1,27 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function Main(){
   let today = new Date()
-    let today_date = today.getDate()
-    let today_day = today.getDay()
-    let today_month = today.getMonth() + 1
-    let today_year = today.getFullYear()
-    const days = [
-      '일요일',
-      '월요일',
-      '화요일',
-      '수요일',
-      '목요일',
-      '금요일',
-      '토요일'
-    ];
+  let today_date = today.getDate()
+  let today_day = today.getDay()
+  let today_month = today.getMonth() + 1
+  let today_year = today.getFullYear()
+  const days = [
+    '일요일',
+    '월요일',
+    '화요일',
+    '수요일',
+    '목요일',
+    '금요일',
+    '토요일'
+  ];
+  let data = localStorage.getItem('todo_list')
   let [todo_data,setTodo_data] = useState(['블로그 작성','깃허브에 1커밋'])
+  useEffect(()=>{
+    if(data){
+      setTodo_data(JSON.parse(data))
+    }
+  },[])
   let [todo_input_data,setTodo_input_data] = useState('')
   return(
     <main id="main">
@@ -39,6 +45,7 @@ function Main(){
               let copy_data = [...todo_data]
               copy_data.unshift(todo_input_data)
               setTodo_data(copy_data)
+              localStorage.setItem('todo_list',JSON.stringify(copy_data))
             }
           }} onChange={(e)=>{
             setTodo_input_data(e.target.value)
@@ -48,6 +55,7 @@ function Main(){
             let copy_data = [...todo_data]
             copy_data.unshift(todo_input_data)
             setTodo_data(copy_data)
+            localStorage.setItem('todo_list',JSON.stringify(copy_data))
           }} className='write_btn'> + </button>
         </div>
       </main>
